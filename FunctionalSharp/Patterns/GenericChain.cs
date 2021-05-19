20ef829f2095679aca244a32cb090983820ceaa2
Matias Iacono <matias.iacono@cucumber.co.nz>
2021-05-20 10:00:02 +1200

    New features on chain pattern

---
+++ FunctionalSharp/Patterns/GenericChain.cs
@@ -0,0 +1,154 @@
+﻿using FunctionalSharp.Validators;
+using System;
+using System.Collections.Generic;
+
+namespace FunctionalSharp.Patterns
+{
+
+    public sealed class GenericChain<T>
+    {
+        public abstract class LinkBase
+        {
+            public abstract void OnExecute(DataCargo data);
+        }
+
+        private sealed class Link : LinkBase
+        {
+            private readonly Action<DataCargo> _action;
+
+            public Link(Action<DataCargo> action)
+            {
+                _action = action;
+            }
+
+            public override void OnExecute(DataCargo data) => _action.Invoke(data);
+        }
+
+        public sealed class Configuration
+        {
+            public bool StopOnFailure { get; }
+            public int RepeatTimesOnFailure { get; }
+
+            public Configuration(
+                bool stopOnFailure = true, 
+                int repeatTimesOnFailure = 0
+            )
+            {
+                StopOnFailure = stopOnFailure;
+                RepeatTimesOnFailure = repeatTimesOnFailure;
+            }
+        }
+
+        public class DataCargo
+        {
+            public T Payload;
+            public bool Cancel { get; set; }
+        }
+
+        private readonly DataCargo _dataCargo;
+        private readonly Configuration _configuration;
+        private readonly List<LinkBase> _chain;
+        private Action<T> _completeAction;
+        private Action<DataCargo, Exception> _errorAction;
+
+        internal GenericChain(T payload, Configuration configuration)
+        {
+            _dataCargo = new DataCargo
+            {
+                Payload = GetPayloadOrInstance(payload)
+            };
+
+            _configuration = configuration ?? new Configuration();
+
+            _chain = new List<LinkBase>();
+        }
+
+        public static GenericChain<T> Create() => new GenericChain<T>(default, null);
+
+        public static GenericChain<T> Create(Configuration configuration) => new GenericChain<T>(default, configuration);
+
+        public static GenericChain<T> Create(T payload, Configuration configuration = null) => new GenericChain<T>(payload, configuration);
+
+        public GenericChain<T> AddLink(Action<DataCargo> action) => AddLink(new Link(action));
+
+        public GenericChain<T> AddLink(LinkBase link)
+        {
+            link.IfNull().Throw(new Exception("Chain Link cannot be null"));
+
+            _chain.Add(link);
+
+            return this;
+        }
+
+        public void Run()
+        {
+            foreach (var link in _chain)
+            {
+                if (RunLinkAndStop(link)) break;
+            }
+
+            _completeAction?.Invoke(_dataCargo.Payload);
+        }
+
+        /// <summary>
+        /// After the chain is fully executed, OnCompleted is called
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public GenericChain<T> OnCompleted(Action<T> action)
+        {
+            _completeAction = action;
+            return this;
+        }
+
+        /// <summary>
+        /// When any link of the chain throws an exception OnError is called
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns>This chain</returns>
+        public GenericChain<T> OnError(Action<DataCargo, Exception> action)
+        {
+            _errorAction = action;
+            return this;
+        }
+
+        private bool RunLinkAndStop(LinkBase link, int attempt = 0)
+        {
+            try
+            {
+                _dataCargo.Cancel = false;
+                link.OnExecute(_dataCargo);
+                return _dataCargo.Cancel;
+            }
+            catch (Exception ex)
+            {
+                _errorAction?.Invoke(_dataCargo, ex);
+
+                if (!_configuration.StopOnFailure && 
+                    _configuration.RepeatTimesOnFailure > 0 &&
+                    attempt < _configuration.RepeatTimesOnFailure - 1)
+                {
+                    return RunLinkAndStop(link, attempt + 1);
+                }
+
+                if (attempt == _configuration.RepeatTimesOnFailure) return true;
+
+                return _configuration.StopOnFailure;
+            }
+        }
+
+        private T GetPayloadOrInstance(T payload)
+        {
+            if (payload != null) return payload;
+
+            var payloadType = typeof(T);
+
+            if (payloadType.GetConstructor(Type.EmptyTypes) != null && !payloadType.IsAbstract)
+            {
+                return Activator.CreateInstance<T>();
+            }
+
+            throw new ArgumentException("Type must be creatable");
+        }
+    }
+}
--- FunctionalSharpTests/Patterns/ChainPatternTests.cs
+++ FunctionalSharpTests/Patterns/ChainPatternTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace FunctionalSharp.Patterns.Tests
 {
@@ -10,16 +11,18 @@ namespace FunctionalSharp.Patterns.Tests
             public int Sum { get; internal set; }
         }
 
+        private abstract class AbstractType { }
+
         [TestMethod()]
         public void When_Chain_IsCreated_ExpectExecution()
         {
-            var chain = Chain<PayloadType>.Create();
+            var chain = GenericChain<PayloadType>.Create();
 
             chain
                 .AddLink(data => data.Payload.Sum = 0)
                 .AddLink(data => data.Payload.Sum += 10)
                 .AddLink(data => data.Payload.Sum += 20)
-                .OnComplete(data => Assert.AreEqual(30, data.Sum))
+                .OnCompleted(data => Assert.AreEqual(30, data.Sum))
                 .Run();
         }
 
@@ -31,16 +34,72 @@ namespace FunctionalSharp.Patterns.Tests
                 Sum = 20
             };
 
-            var chain = Chain<PayloadType>.Create(myLocalPayload);
+            var chain = GenericChain<PayloadType>.Create(myLocalPayload);
 
             chain
                 .AddLink(data => data.Payload.Sum += 10)
                 .AddLink(data => data.Payload.Sum += 10)
                 .AddLink(data => data.Payload.Sum += 20)
-                .OnComplete(data => Assert.AreEqual(60, data.Sum))
+                .OnCompleted(data => Assert.AreEqual(60, data.Sum))
                 .Run();
 
             Assert.AreEqual(60, myLocalPayload.Sum);
+        }
+
+        [TestMethod()]
+        public void When_Chain_IsCreatedWithStringPayload_ExpectRetainValue()
+        {
+            var chain = GenericChain<string>.Create("my payload");
+
+            chain
+                .AddLink(data => data.Payload += " with more data")
+                .AddLink(data => data.Payload += " that should remain")
+                .AddLink(data => data.Payload += " across calls")
+                .OnCompleted(data => Assert.AreEqual("my payload with more data that should remain across calls", data))
+                .Run();
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void When_Chain_IsCreatedWithAbstract_ExpectException()
+        {
+            GenericChain<AbstractType>.Create();
+        }
+
+        [TestMethod()]
+        public void When_Chain_FailAndRepeat_ExpectResults()
+        {
+            var chain = GenericChain<int>.Create(0, 
+                new GenericChain<int>
+                    .Configuration(
+                        stopOnFailure: false, 
+                        repeatTimesOnFailure: 3
+                    )
+                );
+
+            chain
+                .AddLink(data => data.Payload = 10)
+                .AddLink(data => {
+                    data.Payload += 1;
+                    //force exception
+                    throw new Exception();
+                })
+                .OnCompleted(data => Assert.AreEqual(13, data))
+                .Run();
+        }
+
+        [TestMethod()]
+        public void When_Chain_IsCanceled_ExpectResults()
+        {
+            var chain = GenericChain<int>.Create(0);
+
+            chain
+                .AddLink(data => data.Payload = 10)
+                .AddLink(data => data.Payload += 1)
+                .AddLink(data => data.Cancel = true)
+                .AddLink(data => data.Payload += 1)
+                .OnCompleted(data => Assert.AreEqual(11, data))
+                .Run();
         }
     }
 
